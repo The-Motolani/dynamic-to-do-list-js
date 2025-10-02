1357c000d6fdde3106d5477cd5ef7ce66a58e4db
Motolani <alebiosumotolani@gmail.com>
2025-10-02 19:38:29 +0100

    ä dynamic to-do list

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+const addButton = document.getElementById("add-task-btn");
+const taskInput = document.getElementById("task-input");
+const taskList = document.getElementById("task-list");
+
+function addTask(){
+   const taskTest = taskInput.value.trim();
+    if (taskTest === "") {
+      alert("Please enter a task");
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.textContent = taskTest;
+
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "Remove";
+    removeBtn.className = "remove-btn";
+
+    removeBtn.onclick =() => {
+        taskList.removeChild(li);
+    };
+    
+    li.appendChild(removeBtn);
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+
+}
+    addButton.addEventListener("click", addTask);
+
+    taskInput.addEventListener("keypress", (event) => {
+        if (event.key === "Enter") {
+            addTask();
+        }
+    })
+
+});
