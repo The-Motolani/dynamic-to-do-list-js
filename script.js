f4d56c97c0684a616c36d32d4f1514f9d50383b5
Motolani <alebiosumotolani@gmail.com>
2025-10-02 19:48:47 +0100

    ä dynamic to-do list

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const taskInput = document.getElementById("task-input");
 const taskList = document.getElementById("task-list");
 
 function addTask(){
- let taskTest = taskInput.value.trim();
+    taskTest = taskInput.value.trim();
+    
     if (taskTest === "") {
       alert("Please enter a task");
         return;
